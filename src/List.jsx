58fe2a2a892dfc878dcb93e46772fa63dcd822f3
Pawel Skrzypczyk <skrzypczyk.pawel@tlen.pl>
2021-05-28 15:35:50 +0200

    PS-006 add useSelector

--- src/List.jsx
+++ src/List.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Rete from './Rete';
 
-const List = ({rates}) => {
+const List = () => {
+  const rates = useSelector(store => store.rates);
+
   const ratesElements = rates.map(rate => <Rete 
     key={rate.id} 
     {...rate}
@@ -15,10 +17,4 @@ const List = ({rates}) => {
   );
 };
 
-const connectReduxStateToProps = store => ({
-  rates: store.rates,
-})
-
-const ListConsumer = connect(connectReduxStateToProps)(List);
-
-export default ListConsumer;
+export default List;
